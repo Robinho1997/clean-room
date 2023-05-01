7fb4cc835cbf3f437453fd2a1f79806409e6e1f5
Robinho1997 <robin.kempen@gmx.de>
2023-05-01 17:09:27 +0200

    checking if room is cleaned and changing the roomstatus depending on that

--- src/pages/ZimmerStatus.jsx
+++ src/pages/ZimmerStatus.jsx
@@ -1,13 +1,43 @@
 import React, { useContext, useEffect } from "react";
 import "../styles/zimmerstatus.css";
 import { Context } from "../Context";
+import { ref, set } from "firebase/database";
 import { Link } from "react-router-dom";
 import { nanoid } from "nanoid";
 
 function ZimmerStatus() {
-  const { data } = useContext(Context);
+  const { data,db } = useContext(Context);
   const dataArray = data ? Object.values(data) : [];
   let elements;
+console.log(dataArray)
+
+  //check if all aufgaben erledigt is true and all badezimmerAufgaben erledigt is true , then set zimmerstatus to true
+  //if not set zimmerstatus to false
+useEffect(() => {
+  if (data) {
+    dataArray.forEach((room) => {
+      let aufgabenErledigt = true;
+      let badezimmerAufgabenErledigt = true;
+      let reinigunsstatus = true;
+      room.aufgaben.forEach((aufgabe) => {
+        if (!aufgabe.erledigt) {
+          aufgabenErledigt = false;
+        }
+      });
+      room.badezimmerAufgaben.forEach((aufgabe) => {
+        if (!aufgabe.erledigt) {
+          badezimmerAufgabenErledigt = false;
+        }
+      });
+      if (!aufgabenErledigt || !badezimmerAufgabenErledigt) {
+        reinigunsstatus = false;
+      }
+      room.reinigunsstatus = reinigunsstatus;
+    });
+    set(ref(db, "rooms"), data);
+  }
+}, [])
+
   
   if (data) {
     elements = dataArray.map((room) => {
@@ -15,7 +45,7 @@ function ZimmerStatus() {
         <Link
           key={nanoid()}
           to={`zimmer/${room.raumnummer}`}
-          className="room-number"
+          className={room.reinigunsstatus ? "cleaned" : "dirty"}
         >
           {room.raumnummer}
         </Link>
